3cde36c119281040ab7dea7238abdb50eae6427e
gmngomez021 <gmngomez021@student.wethinkcode.co.za>
2022-10-17 16:21:07 +0200

    Social Media App

---
+++ Verve_backend/verveaesthetics/schemas/user.js
@@ -0,0 +1,18 @@
+// Document for the user name and also the image to be added by user
+export default {
+    name: 'user',
+    title: 'User',
+    type: 'document',
+    fields: [
+        {
+            name: 'userName',
+            title: 'UserName',
+            type: 'string'
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'string'
+        },
+    ]
+}
